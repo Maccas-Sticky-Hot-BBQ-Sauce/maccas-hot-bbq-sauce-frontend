7161f5afa729ea51cabdc9928f2b02cb63bb6dff
Ferdi Sungkar <ferdisgk@gmail.com>
2022-08-29 18:38:01 +1000

    update: added time and logo on appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,50 +32,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  DateTime now = DateTime.now();
+  late String _time;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    _time = _formatDateTime(DateTime.now());
+    Timer.periodic(const Duration(seconds: 1), (Timer t) => _getTime());
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.white,
+        leading: Image.asset(
+          'assets/images/TRANSLink.png',
+          width: 217,
+          height: 60,
+        ),
+        actions: <Widget>[
+          Text(
+            _time,
+            style: const TextStyle(
+                fontFamily: 'helvetica-neue',
+                fontWeight: FontWeight.w700,
+                fontSize: 40,
+                color: Colors.black),
+          ),
+        ],
+        toolbarHeight: 140,
       ),
       body: Container(
         padding: const EdgeInsets.all(30.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
@@ -108,6 +83,10 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
+                  const Icon(
+                    Icons.keyboard_arrow_left_sharp,
+                    size: 100.0,
+                  ),
                   ElevatedButton(
                     onPressed: () {
                       return;
@@ -133,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     style: ElevatedButton.styleFrom(
                         primary: Colors.white,
                         onPrimary: Colors.black,
-                        fixedSize: const Size(165, 74),
+                        fixedSize: const Size(109, 74),
                         side: const BorderSide(color: Colors.black, width: 2)),
                     child: const Text(
                       '402',
@@ -151,7 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     style: ElevatedButton.styleFrom(
                         primary: Colors.white,
                         onPrimary: Colors.black,
-                        fixedSize: const Size(165, 74),
+                        fixedSize: const Size(109, 74),
                         side: const BorderSide(color: Colors.black, width: 2)),
                     child: const Text(
                       '411',
@@ -169,7 +148,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     style: ElevatedButton.styleFrom(
                         primary: Colors.white,
                         onPrimary: Colors.black,
-                        fixedSize: const Size(165, 74),
+                        fixedSize: const Size(109, 74),
                         side: const BorderSide(color: Colors.black, width: 2)),
                     child: const Text(
                       '412',
@@ -180,17 +159,58 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                   ),
+                  ElevatedButton(
+                    onPressed: () {
+                      return;
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        onPrimary: Colors.black,
+                        fixedSize: const Size(109, 74),
+                        side: const BorderSide(color: Colors.black, width: 2)),
+                    child: const Text(
+                      '428',
+                      style: TextStyle(
+                        fontFamily: 'helvetica-neue',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 36,
+                      ),
+                    ),
+                  ),
+                  const Icon(
+                    Icons.keyboard_arrow_right_sharp,
+                    size: 100.0,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Icon(
+                    Icons.keyboard_arrow_up_sharp,
+                    size: 100.0,
+                  ),
                 ],
               ),
             )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _getTime() {
+    final DateTime now = DateTime.now();
+    final String formattedDateTime = _formatDateTime(now);
+    setState(() {
+      _time = formattedDateTime;
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return DateFormat('hh:mm').format(dateTime);
   }
 }
