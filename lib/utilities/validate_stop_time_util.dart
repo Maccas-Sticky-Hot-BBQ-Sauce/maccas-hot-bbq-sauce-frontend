d4d75cb0756055d2a11a62fdb920f9686ef543d0
Faraz <faraz.abishamirza@gmail.com>
2022-10-13 18:50:15 +1000

    Create validate_stop_time_util

---
+++ lib/utilities/validate_stop_time_util.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:maccas_sticky_hot_bbq_sauce/models/calendar_model.dart';
+import 'package:maccas_sticky_hot_bbq_sauce/models/exception_model.dart';
+import 'package:maccas_sticky_hot_bbq_sauce/models/stop_time_model.dart';
+
+class ValidateStopTimeUtil {
+
+  static bool valid(StopTimeModel stopTime){
+    for (ExceptionModel exception in stopTime.trip.exceptions ?? []) {
+        if (DateUtils.isSameDay(exception.date, DateTime.now())) {
+          return false;
+        }
+    }
+    if (runningToday(stopTime.trip.calendar)){
+      return afterNow(stopTime);
+    } else {
+      return false;
+    }
+  }
+
+  static bool runningToday (CalendarModel calendar){
+    String today = DateFormat('EEEE').format(DateTime.now()).toUpperCase();
+    return (!calendar.days.contains(today)) ? false : true;
+  }
+
+  static bool afterNow(StopTimeModel stopTime){
+    return stopTime.departure.isAfter(DateTime.now()) ? true : false;
+  }
+}
