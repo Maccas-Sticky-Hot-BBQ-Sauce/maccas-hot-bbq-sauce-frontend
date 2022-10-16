3c5370a83e3707bb081c7435509ffbaad7508acb
Faraz <faraz.abishamirza@gmail.com>
2022-10-16 15:13:23 +1000

    Create route_stops.dart and stop_card.dart

---
+++ lib/screens/route_stops.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:maccas_sticky_hot_bbq_sauce/models/trip_model.dart';
+import 'package:maccas_sticky_hot_bbq_sauce/widgets/cards/stop_card.dart';
+
+import '../models/stop_time_model.dart';
+
+class RouteStops extends StatefulWidget {
+  final TripModel? trip;
+  const RouteStops({Key? key, required this.trip}) : super(key: key);
+
+  @override
+  State<RouteStops> createState() => _RouteStopsState();
+}
+
+class _RouteStopsState extends State<RouteStops> {
+
+  @override
+  Widget build(BuildContext context) {
+    TripModel? trip = widget.trip;
+    return SingleChildScrollView(
+      child: Container(
+        margin: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
+                    child: const Icon(
+                      Icons.arrow_back_rounded,
+                      size: 64,
+                    )
+                  ),
+                  const Text(
+                    "Details",
+                    style: TextStyle(
+                        fontFamily: 'helvetica-neue',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 44),
+                  )
+                ],
+              ),
+            ),
+            for (StopTimeModel stopTimes in trip!.stopTimes!) ...[
+              StopCard(
+                  stopName: stopTimes.stop!.name,
+                  stoppingTime: stopTimes.arrival,
+              ),
+            ],
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 0, 48.53, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Icon(
+                    Icons.keyboard_arrow_down_sharp,
+                    size: 100.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/cards/stop_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class StopCard extends StatelessWidget {
+  final String stopName;
+  final DateTime stoppingTime;
+
+  const StopCard({
+    Key? key,
+    required this.stopName,
+    required this.stoppingTime
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 35, 0, 35),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(
+                stopName,
+                style: const TextStyle(
+                  fontFamily: 'helvetica-neue',
+                  fontWeight: FontWeight.w300,
+                  fontSize: 32,
+                  ),
+                )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 32.27, 0),
+                child: Text(
+                        DateFormat.Hm().format(stoppingTime),
+                        style: const TextStyle(
+                          fontFamily: 'helvetica-neue',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 32,
+                          ),
+                        )
+                    )
+                  ],
+          ),
+        ],
+      ),
+    );
+  }
+}
